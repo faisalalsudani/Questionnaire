bf662dc806cba06a84e5934b90487c1a96873a95
faisalalsudani <f4desiign@gmail.com>
2017-11-24 10:31:11 +0100

    show and hide next submit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,20 @@
 //= require jquery_ujs
 //= require fuelux
 //= require_tree .
+
+$(document).ready(function() {
+
+  $('.btn-submit').hide();
+  var totalQuestion = $('.step-pane').length;
+  var currentStep = $('#myWizard').on('changed.fu.wizard', function(evt, item) {
+    console.log(item.step);
+    if (item.step == totalQuestion) {
+      $('.btn-submit').show();
+      $('.btn-next').hide();
+    }
+  });
+
+
+
+
+ });
