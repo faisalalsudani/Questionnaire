1ab50ecedcccce4fa1633d448d2c2e6d9a8951e7
faisalalsudani <f4desiign@gmail.com>
2017-11-25 12:30:45 +0100

    fixing next prev buttons with the timer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,40 @@
 $(document).ready(function() {
 
   $('.btn-submit').hide();
+  $('.btn-prev').hide();
   var totalQuestion = $('.step-pane').length;
-  var currentStep = $('#myWizard').on('changed.fu.wizard', function(evt, item) {
+  var currentStep = $('#questionFrom').on('changed.fu.wizard', function(evt, item) {
     console.log(item.step);
-    if (item.step == totalQuestion) {
-      $('.btn-submit').show();
-      $('.btn-next').hide();
+
+    if (item.step >= 3) {
+      $('#timer').remove();
+      $('.btn-prev').show();
+    } else {
+      $('.btn-prev').hide();
     }
+
+  });
+
+
+
+
+  var timer;
+  function myTimer(sec) {
+    if (timer) clearInterval(timer);
+    timer = setInterval(function() {
+      $('#timer').text(sec--);
+      if (sec == -1) {
+        clearInterval(timer);
+        $('#questionFrom').wizard('next');
+        $(function() {
+          myTimer(3);
+        });
+      }
+    }, 1000);
+  }
+
+  $(function() {
+    myTimer(3);
   });
 
 
